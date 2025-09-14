2b6522077a14c0eb3157538ca2c092bb3eff4b1a
Ahmed Massoud <ahmed14massoud2021@gmail.com>
2025-09-14 13:39:11 +0300

    updta

--- conn.php
+++ conn.php
@@ -3,8 +3,8 @@
  $username = 'epiz_34278868';
  $password = 'rvU6NBt1Bkp';
  $database = 'epiz_34278868_massBook';
-//  $conn = mysqli_connect($hostname, $username, $password, $database);
- $conn = mysqli_connect("127.0.0.1", "root", "", "Mass-book");
+ $conn = mysqli_connect($hostname, $username, $password, $database);
+//  $conn = mysqli_connect("127.0.0.1", "root", "", "Mass-book");
 
 
 ?>
